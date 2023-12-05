93634393799bd7ea4bf3c3f624c0b1f3a2586265
Vladimir <vovakovalev2016@icloud.com>
2023-12-05 18:08:49 +0300

    Initial Commit

---
+++ Lab5/Lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5App.swift
+//  Lab5
+//
+//  Created by user on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
